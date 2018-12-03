9d7e65b72038f9b728bec60bb2b34d64699dccd7
Luis ivan Sainz romero <thesainz94@gmail.com>
2018-12-02 19:52:14 -0600

    ok

--- server/models/temperatura.js
+++ server/models/temperatura.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Temperatura) {
-
+  Temperatura.remoteMethod('postValue', {
+    accepts: [{arg: 'value', type: 'number', required: true}],
+    returns: [{arg: 'status', type: 'string'}],
+  });
+  Temperatura.postValue = function(value, cb) {
+    return Temperatura.create({value}, function(error, Temperatura) {
+      if (error) console.log(error);
+      return cb(null, 'ok');
+    });
+  };
 };
